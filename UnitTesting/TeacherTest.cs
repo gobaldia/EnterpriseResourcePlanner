635aa1928e4a046d5a9cfa435880b3d9ef638e89
SANTIPC\santi <santidiaz.uy@gmail.com>
2017-10-09 23:54:43 -0300

    CreateTeacher test refactor.

--- UnitTesting/TeacherTest.cs
+++ UnitTesting/TeacherTest.cs
@@ -21,11 +21,18 @@ namespace UnitTesting
             Assert.AreEqual(expectedName, teacher.GetName());
             Assert.AreEqual(expectedLastName, teacher.GetLastName());
 
-            Assert.AreEqual(actualSubjects.Count, expectedSubjects.Count);
-            for (var index = 0; index < actualSubjects.Count; index++)
+            this.CompareSubjects(actualSubjects, expectedSubjects);
+        }
+
+        #region Extra Methods
+        private void CompareSubjects(List<Subject> real, List<Subject> toBeCompareWith)
+        {
+            Assert.AreEqual(real.Count, toBeCompareWith.Count);
+            for (var index = 0; index < real.Count; index++)
             {
-                Assert.AreEqual(actualSubjects[index], actualSubjects[index]);
+                Assert.AreEqual(real[index], toBeCompareWith[index]);
             }
         }
+        #endregion
     }
 }
