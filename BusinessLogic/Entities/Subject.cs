adc8b82c37096b36840ed430c4f506e9dc532681
MININT-1VGNMJM\Gabi <gobaldia@gmail.com>
2017-10-10 19:41:42 -0300

    Pass tests that failed before

--- BusinessLogic/Entities/Subject.cs
+++ BusinessLogic/Entities/Subject.cs
@@ -8,6 +8,40 @@ namespace BusinessLogic.Entities
 {
     public class Subject
     {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Teacher> Teachers { get; set; }
 
+        public Subject()
+        {
+            this.Code = 0;
+            this.Name = string.Empty;
+            this.Students = new List<Student>();
+            this.Teachers = new List<Teacher>();
+        }
+
+        public Subject(int code, string name)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Students = new List<Student>();
+            this.Teachers = new List<Teacher>();
+        }
+
+        public int GetCode()
+        {
+            return this.Code;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.GetCode() == (((Subject)obj).GetCode());
+        }
     }
 }
