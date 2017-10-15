7141e1fe65a8cec3c4781b52e08d9dc2cbd3a40d
SANTIPC\santi <santidiaz.uy@gmail.com>
2017-10-15 02:19:55 -0300

    CreateStudent test fail.

---
+++ UnitTesting/StudentTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTesting.Utilities;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void CreateStudent()
+        {
+            Student student = new Student();
+
+            string expectedName = string.Empty;
+            string expectedLastName = string.Empty;
+            int expectedStudentNumber = 1;
+            List<Subject> expectedSubjects = new List<Subject>();
+            List<Subject> actualSubjects = student.GetSubjects();
+
+            Assert.AreEqual(expectedName, student.GetName());
+            Assert.AreEqual(expectedLastName, student.GetLastName());
+            Assert.AreEqual(expectedStudentNumber, student.GetStudentNumber());
+
+            Assert.IsTrue(Utility.CompareLists(actualSubjects, expectedSubjects));
+        }
+    }
+}
