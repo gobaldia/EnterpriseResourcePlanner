08832ed95e5988e28b636c506b71adcdf9fad4a5
SANTIPC\santi <santidiaz.uy@gmail.com>
2017-10-15 15:32:17 -0300

    ThrowInvalidFormatWhenTrySetInvalidLatitud test passed.

--- UnitTesting/LocationTest.cs
+++ UnitTesting/LocationTest.cs
@@ -1,4 +1,5 @@
 ﻿using CoreEntities.Entities;
+using CoreEntities.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,27 @@ namespace UnitTesting
             Location location = new Location(latitud, longitud);
 
             Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ThrowInvalidFormatWhenTrySetInvalidLatitud()
+        {
+            try
+            {
+                double latitud = 222;
+                double longitud = -2.2;
+                Location location = new Location(latitud, longitud);
+
+                Assert.Fail();
+            }
+            catch (CoreException ex)
+            {
+                Assert.IsTrue(ex.Message.Equals("Invalid latitud format."));
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
         }
     }
 }
