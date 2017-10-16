e88cba886b9574255e506eef50b8cc5e0381b065
MININT-1VGNMJM\Gabi <gobaldia@gmail.com>
2017-10-16 03:38:09 -0300

    Design add form

--- VehicleModuleUI/AddVehicle/AddVehicleForm.Designer.cs
+++ VehicleModuleUI/AddVehicle/AddVehicleForm.Designer.cs
@@ -28,11 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelAddVehicleTitle = new System.Windows.Forms.Label();
+            this.labelRegistration = new System.Windows.Forms.Label();
+            this.labelCapacity = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.labelSuccess = new System.Windows.Forms.Label();
+            this.textBoxRegistration = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelAddVehicleTitle
+            // 
+            this.labelAddVehicleTitle.AutoSize = true;
+            this.labelAddVehicleTitle.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAddVehicleTitle.Location = new System.Drawing.Point(13, 13);
+            this.labelAddVehicleTitle.Name = "labelAddVehicleTitle";
+            this.labelAddVehicleTitle.Size = new System.Drawing.Size(168, 23);
+            this.labelAddVehicleTitle.TabIndex = 0;
+            this.labelAddVehicleTitle.Text = "Add new Vehicle";
+            // 
+            // labelRegistration
+            // 
+            this.labelRegistration.AutoSize = true;
+            this.labelRegistration.Location = new System.Drawing.Point(17, 59);
+            this.labelRegistration.Name = "labelRegistration";
+            this.labelRegistration.Size = new System.Drawing.Size(63, 13);
+            this.labelRegistration.TabIndex = 1;
+            this.labelRegistration.Text = "Registration";
+            // 
+            // labelCapacity
+            // 
+            this.labelCapacity.AutoSize = true;
+            this.labelCapacity.Location = new System.Drawing.Point(17, 121);
+            this.labelCapacity.Name = "labelCapacity";
+            this.labelCapacity.Size = new System.Drawing.Size(48, 13);
+            this.labelCapacity.TabIndex = 2;
+            this.labelCapacity.Text = "Capacity";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(17, 138);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(17, 222);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(17, 252);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 6;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(17, 203);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 16);
+            this.labelError.TabIndex = 7;
+            // 
+            // labelSuccess
+            // 
+            this.labelSuccess.AutoSize = true;
+            this.labelSuccess.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSuccess.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.labelSuccess.Location = new System.Drawing.Point(17, 202);
+            this.labelSuccess.Name = "labelSuccess";
+            this.labelSuccess.Size = new System.Drawing.Size(0, 16);
+            this.labelSuccess.TabIndex = 8;
+            // 
+            // textBoxRegistration
+            // 
+            this.textBoxRegistration.Location = new System.Drawing.Point(17, 76);
+            this.textBoxRegistration.Name = "textBoxRegistration";
+            this.textBoxRegistration.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRegistration.TabIndex = 9;
+            // 
+            // AddVehicleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 392);
+            this.Controls.Add(this.textBoxRegistration);
+            this.Controls.Add(this.labelSuccess);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.labelCapacity);
+            this.Controls.Add(this.labelRegistration);
+            this.Controls.Add(this.labelAddVehicleTitle);
+            this.Name = "AddVehicleForm";
             this.Text = "AddVehicleForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelAddVehicleTitle;
+        private System.Windows.Forms.Label labelRegistration;
+        private System.Windows.Forms.Label labelCapacity;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Label labelSuccess;
+        private System.Windows.Forms.TextBox textBoxRegistration;
     }
 }
