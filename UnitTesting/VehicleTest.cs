ad53ac46d9c475c4fdbcff4708bb046d0f21fcf3
MININT-1VGNMJM\Gabi <gobaldia@gmail.com>
2017-10-15 23:57:50 -0300

    Wrote first tests for add-vehicle. All of them fail

---
+++ UnitTesting/VehicleTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace UnitTesting
+{
+    [TestClass]
+    class VehicleTest
+    {
+        [TestMethod]
+        public void CreateVehicleWithParameters()
+        {
+            var expectedRegistration = "SBA0122";
+            var expectedCapacity = 10;
+
+            Vehicle vehicle = new Vehicle(expectedRegistration, expectedCapacity);
+
+            Assert.AreEqual(expectedRegistration, vehicle.Registration);
+            Assert.AreEqual(expectedCapacity, vehicle.Capacity);
+        }
+
+        [TestMethod]
+        public void CreateVehicleWithoutParameters()
+        {
+            Vehicle vehicle = new Vehicle();
+
+            Assert.AreEqual(expectedRegistration, "AAA0000");
+            Assert.AreEqual(expectedCapacity, 1);
+        }
+
+        [TestMethod]
+        public void CreateVehicleCapacityLessOrEqualThanZero()
+        {
+            var expectedRegistration = "SBA0122";
+            var expectedCapacity = 0;
+
+            Vehicle vehicle = new Vehicle(expectedRegistration, expectedCapacity);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
