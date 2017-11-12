d41d3e7c7520015dda6162e2c514ffe8a2aa5eb0
MININT-1VGNMJM\Gabi <gobaldia@gmail.com>
2017-11-12 17:53:57 -0300

    Add more tests that fail

--- UnitTesting/ActivityTest.cs
+++ UnitTesting/ActivityTest.cs
@@ -29,5 +29,21 @@ namespace UnitTesting
             Assert.AreEqual(activity.Date = expectedDate);
             Assert.AreEqual(activity.Cost = expectedCost);
         }
+
+        [TestMethod]
+        public void CreateActivityWithParameters()
+        {
+            var expectedName = "Yoga";
+            var expectedId = 1;
+            var expectedDate = new DateTime(2017, 17, 11, 10, 0, 0);
+            var expectedCost = 100;
+
+            Activity activity = new Activity(expectedName, expectedId, expectedDate, expectedCost);
+
+            Assert.AreEqual(activity.Name = expectedName);
+            Assert.AreEqual(activity.Id = expectedId);
+            Assert.AreEqual(activity.Date = expectedDate);
+            Assert.AreEqual(activity.Cost = expectedCost);
+        }
     }
 }
