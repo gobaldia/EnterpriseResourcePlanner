0cadb0746e2effcb2f66f8c19a3af85335cccd9d
MININT-1VGNMJM\Gabi <gobaldia@gmail.com>
2017-11-12 17:50:21 -0300

    Wrote tests for Add Activity. All tests fail

---
+++ UnitTesting/ActivityTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class ActivityTest
+    {
+        [TestMethod]
+        public void CreateActivity()
+        {
+            var expectedName = "Yoga";
+            var expectedId = 1;
+            var expectedDate = new DateTime(2017, 17, 11, 10, 0, 0);
+            var expectedCost = 100;
+
+            Activity activity = new Activity();
+            activity.Name = expectedName;
+            activity.Id = expectedId;
+            activity.Date = expectedDate;
+            activity.Cost = expectedCost;
+
+            Assert.AreEqual(activity.Name = expectedName);
+            Assert.AreEqual(activity.Id = expectedId);
+            Assert.AreEqual(activity.Date = expectedDate);
+            Assert.AreEqual(activity.Cost = expectedCost);
+        }
+    }
+}
