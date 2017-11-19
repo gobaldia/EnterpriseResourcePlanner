abc7ebb8f113a8c124418c437f1c7ce01ca356e6
SANTIPC\santi <santidiaz.uy@gmail.com>
2017-11-19 15:57:00 -0300

    Added DummyPersistance in order to generate TDD

---
+++ DummyPersistance/DummyProvider.cs
@@ -0,0 +1,58 @@
+﻿using CoreLogic;
+using CoreLogic.Interfaces;
+using DummyPersistance.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyPersistance
+{
+    public class DummyProvider
+    {
+        private ITeacherLogic teachersLogic;
+        private IStudentLogic studentsLogic;
+        private ISubjectLogic subjectsLogic;
+        //private IVehicleLogic vehiclesLogic;
+
+        #region Singleton
+        // Variable estática para la instancia, se necesita utilizar una función lambda ya que el constructor es privado.
+        private static readonly Lazy<DummyProvider> instance = new Lazy<DummyProvider>(() => new DummyProvider());
+        private DummyProvider()
+        {
+            this.teachersLogic = new TeacherLogic(new TeacherDummyPersistance());
+            this.studentsLogic = new StudentLogic(new StudentDummyPersistance());
+            this.subjectsLogic = new SubjectLogic(new SubjectDummyPersistance());
+            //this.vehiclesLogic = new VehicleLogic(new VehiclePersistance());
+        }
+        public static DummyProvider GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+        #endregion
+
+        public ITeacherLogic GetTeacherOperations()
+        {
+            return this.teachersLogic;
+        }
+
+        public ISubjectLogic GetSubjectOperations()
+        {
+            return this.subjectsLogic;
+        }
+
+        public IStudentLogic GetStudentOperations()
+        {
+            return this.studentsLogic;
+        }
+
+        //public IVehicleLogic GetVehicleOperations()
+        //{
+        //    return this.vehiclesLogic;
+        //}
+    }
+}
---
+++ DummyPersistance/Implementations/StudentDummyPersistance.cs
@@ -0,0 +1,65 @@
+﻿using DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreEntities.Entities;
+
+namespace DummyPersistance.Implementations
+{
+    public class StudentDummyPersistance : IStudentPersistance
+    {
+        private List<Student> dummySystemStudents = SystemDummyData.GetInstance.GetStudents();
+        public void AddStudent(Student newStudent)
+        {
+            dummySystemStudents.Add(newStudent);
+        }
+
+        public void DeleteStudent(Student studentToDelete)
+        {
+            dummySystemStudents.Remove(studentToDelete);
+        }
+
+        public int GetNextStudentNumber()
+        {
+            int studentNumber = 0;
+            if (dummySystemStudents?.Count() > 0)
+            {
+                var student = dummySystemStudents.OrderByDescending(item => item.StudentNumber).First();
+                studentNumber = student.StudentNumber;
+            }
+            return ++studentNumber;
+        }
+
+        public Student GetStudentByDocumentNumber(string documentNumber)
+        {
+            return dummySystemStudents.Find(s => s.Document.Equals(documentNumber));
+        }
+
+        public Student GetStudentByNumber(int studentNumber)
+        {
+            return dummySystemStudents.Find(s => s.StudentNumber.Equals(studentNumber));
+        }
+
+        public List<Student> GetStudents(bool bringSubjects = false)
+        {
+            return dummySystemStudents;
+        }
+
+        public bool IsStudentInSystem(string documentNumber)
+        {
+            return dummySystemStudents.Exists(s => s.Document.Equals(documentNumber));
+        }
+
+        public void ModifyStudent(Student studentToModify)
+        {
+            Student systemStudent = GetStudentByDocumentNumber(studentToModify.Document);
+            systemStudent.SetName(studentToModify.Name);
+            systemStudent.SetLastName(studentToModify.LastName);
+            systemStudent.SetPickUpService(studentToModify.HavePickUpService);
+            systemStudent.SetLocation(studentToModify.Location);
+            systemStudent.Subjects = studentToModify.Subjects;
+        }
+    }
+}
